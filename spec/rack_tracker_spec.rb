32a49a8279bc1ea7c20edb3e84266a20ef5b4092
Yomi Colledge <baphled@boodah.net>
2012-12-28 06:34:57 +0000

    Add pending spec as a note of something to do later

--- spec/rack_tracker_spec.rb
+++ spec/rack_tracker_spec.rb
@@ -2,10 +2,15 @@ require "spec_helper"
 
 describe Rack::Tracker do
   describe "#plugins" do
-    it "provides a list of plugins available"
-
     it "provides plugins used by default" do
       Rack::Tracker.plugins.should eql ['request_tracker']
+    end
+  end
+
+  describe "#available_plugins" do
+    it "provides a list of plugins available" do
+      pending 'Yet to implement...'
+      Rack::Tracker.available_plugins.should eql ['request_tracker', 'request_time_tracker']
     end
   end
 end
