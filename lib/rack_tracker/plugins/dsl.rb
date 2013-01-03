51c08981e07df2dfc3af27c18104ba07b3785835
Yomi Colledge <baphled@boodah.net>
2013-01-03 19:42:46 +0000

    Should be able to define a class variables and its accompanying methods

--- lib/rack_tracker/plugins/dsl.rb
+++ lib/rack_tracker/plugins/dsl.rb
@@ -11,6 +11,21 @@ module Rack
         def define_plugin_module plugin_name
           """
           module Rack::Tracker::Plugins::#{plugin_name.to_s.camelize}
+            module ClassMethods
+              @@total_requests = 0
+
+              def total_requests= amount
+                @@total_requests += amount
+              end
+
+              def total_requests
+                @@total_requests
+              end
+            end
+
+            def self.included(receiver)
+              receiver.extend ClassMethods
+            end
           end
           """
         end
--- spec/rack_tracker/plugins/dsl_spec.rb
+++ spec/rack_tracker/plugins/dsl_spec.rb
@@ -1,16 +1,26 @@
 require 'spec_helper'
+require "#{File.dirname(__FILE__)}/../../../lib/plugins/repository_revision"
 
 describe Rack::Tracker::Plugins::DSL do
   describe "#new" do
     it "allows me to create a new plugin" do
       Rack::Tracker::Plugins::DSL.new :suttin_cool do
       end
-
       expect {
         Rack::Tracker::Plugins::SuttinCool
       }.to_not raise_error NameError
     end
-    it "allows me to define a class method for the plugin"
+
+    it "allows me to define a class method for the plugin" do
+      Rack::Tracker::Plugins::DSL.new :a_cool_plugin do
+        Rack::Tracker.plugin_variables :last_request_time => nil
+      end
+      class RandomPluginWrapper
+        include Rack::Tracker::Plugins::ACoolPlugin
+      end
+      RandomPluginWrapper.total_requests.should eql 0
+    end
+
     it "allows me to add an event hook"
     it "allows me to add plugin variables"
   end
