3abdb6f138fac2e5d218e01aa318fc903f74ce15
Yomi Colledge <baphled@boodah.net>
2013-01-04 13:46:07 +0000

    Resolve issue with class methods not being set properly

--- lib/rack_tracker/macros.rb
+++ lib/rack_tracker/macros.rb
@@ -4,8 +4,8 @@ module Rack
       module ClassMethods
         def plugin_variables variable_hashes
           variable_hashes.reduce('') do |s, (variable, value)|
-            """
-              module Rack::Tracker::Plugins::ACoolPlugin
+            plugin_class_methods = """
+              module Rack::Tracker::Plugins::#{Rack::Tracker::DSL.currently_defining.to_s.camelize}
                 module ClassMethods
                   @@#{variable} = #{value}
 
@@ -20,8 +20,13 @@ module Rack
                     @@#{variable} = value
                   end
                 end
+
+                def self.included(receiver)
+                  receiver.extend ClassMethods
+                end
               end
             """
+            eval plugin_class_methods
           end
         end
       end
--- lib/rack_tracker/plugins/dsl.rb
+++ lib/rack_tracker/plugins/dsl.rb
@@ -19,8 +19,9 @@ module Rack
         end
 
         def initialize plugin_name, &block
-          eval define_plugin_module plugin_name, &block
           @@currently_defining = plugin_name
+          yield Rack::Tracker::DSL
+          eval define_plugin_module plugin_name, &block
         end
 
         protected
@@ -28,13 +29,6 @@ module Rack
         def define_plugin_module plugin_name, &block
           """
           module Rack::Tracker::Plugins::#{plugin_name.to_s.camelize}
-            module ClassMethods
-              #{block.call}
-            end
-
-            def self.included(receiver)
-              receiver.extend ClassMethods
-            end
           end
           """
         end
