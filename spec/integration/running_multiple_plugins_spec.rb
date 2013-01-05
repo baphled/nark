253f4e0d8f804d47bfe0676d79c75be72b33a7fe
Yomi Colledge <baphled@boodah.net>
2013-01-05 17:32:13 +0000

    Remove the old plugin modules. The DSL will be uesd to define plugins from now on.

--- spec/integration/running_multiple_plugins_spec.rb
+++ spec/integration/running_multiple_plugins_spec.rb
@@ -11,24 +11,28 @@ describe "Running multiple plugins" do
 
   describe "running two plugins that use the before hook" do
     before :each do
-      Rack::Tracker::Plugins::DSL.add_hook :before_call do |env|
-        Rack::Tracker.total_requests += 1
+      Rack::Tracker::DSL.new :request_times do |plugin|
+        plugin.variables :last_request_time => nil
+
+        plugin.add_hook :before_call do |env|
+          @start_time = Time.now
+        end
+
+        plugin.add_hook :after_call do |env|
+          Rack::Tracker.last_request_time = (Time.now - @start_time)
+        end
       end
 
-      # FIXME: These get automatically set when requiring the plugin. Need another solution for dealing with these
-      Rack::Tracker::Plugins::DSL.add_hook :before_call do |env|
-        @start_time = Time.now
-      end
+      Rack::Tracker::DSL.new :requests do |plugin|
+        plugin.variables :total_requests => 0
 
-      Rack::Tracker::Plugins::DSL.add_hook :after_call do |env|
-        request_time = (Time.now - @start_time)
-        Rack::Tracker.last_request_time = request_time
-        Rack::Tracker.request_times << {:url => env['PATH_INFO'], :request_time => request_time}
+        plugin.add_hook :before_call do |env|
+          Rack::Tracker.total_requests += 1
+        end
       end
     end
 
     it "can track the number of requests and the amount of time the request took" do
-      # TODO: Refactor this once plugins no longer needed to be define conventionally
       Rack::Tracker.add_plugins [:request_times, :requests ]
       get '/'
       Rack::Tracker.last_request_time.should be_within(0.1).of(0.1)
--- spec/plugins/contract_spec.rb
+++
@@ -1,6 +0,0 @@
-require "spec_helper"
-
-describe "Plugin Contracts" do
-  it "must have a filter or method call"
-  it "adds its name to the plugins module list"
-end
