1689d056ee7f362323a0441b46e79a147eb1e912
Yomi Colledge <baphled@boodah.net>
2013-01-18 20:57:35 +0000

    Further refactoring the EventHandler

--- lib/nark/event_handler.rb
+++ lib/nark/event_handler.rb
@@ -8,14 +8,16 @@ module Nark
       # triggered.
       #
       def trigger event, env
-        triggered_events(event).each do |hook|
-          Nark.class_eval 'hook[:plugin_method].call env'
+        triggered(event).each do |triggered_event|
+          Nark.class_eval 'triggered_event[:plugin_method].call env'
         end
       end
 
-      def triggered_events event
-        self.class.events.select do |listener|
-          listener[:hook].to_sym == event.to_sym
+      protected
+
+      def triggered event
+        self.class.events.select do |event_hook|
+          event_hook[:hook].to_sym == event.to_sym
         end
       end
     end
