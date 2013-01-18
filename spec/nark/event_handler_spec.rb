44de9fdb8a9ac8106bbd2e3e78e4632bb8e5f289
Yomi Colledge <baphled@boodah.net>
2013-01-18 20:44:07 +0000

    Cover the trigger_hook functionality

--- spec/nark/event_handler_spec.rb
+++ spec/nark/event_handler_spec.rb
@@ -4,6 +4,17 @@ describe Nark::EventHandler do
   class Wrapper
     include Nark::EventHandler
   end
+
+  describe "#trigger_hook" do
+    it "triggers before and after hooks at least twice" do
+      env = {}
+      wrapper = Wrapper.new
+      Wrapper.events << {hook: :before_call, plugin_method: Proc.new {}, plugin: 'new_plugin'}
+      wrapper.should_receive(:trigger_hook)
+      wrapper.trigger_hook :before_call, env
+    end
+  end
+
   describe "#events" do
     it "stores a list of events" do
       Wrapper.events.should be_an Array
