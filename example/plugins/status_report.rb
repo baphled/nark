4b08855af82bb9c0ad4e52e616ab0045f129dcc3
Yomi Colledge <baphled@boodah.net>
2013-01-18 20:11:47 +0000

    No longer use the after_response hook

--- example/plugins/status_report.rb
+++ example/plugins/status_report.rb
@@ -1,7 +1,7 @@
 Nark::Plugin.define :status_report do |plugin|
   plugin.variables :statues => []
 
-  plugin.add_hook :after_response do |status_code, header, body, env|
+  plugin.add_hook :after_call do |status_code, header, body, env|
     plugin.statues << {:status => status_code, :path => env['PATH_INFO']}
   end
 end
