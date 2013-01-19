f33e499fa56c3e7d2824d75f13545cd26a21caab
Yomi Colledge <baphled@boodah.net>
2013-01-19 09:16:35 +0000

    Rename HTTP based reporting steps file

---
+++ features/step_definitions/http_reporting_steps.rb
@@ -0,0 +1,26 @@
+When /^the API is started$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I visit "(.*?)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the "(.*?)" should be \[\]$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+Given /^I am using the API service$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I start my application$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the API service should be available$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should be able to access "(.*?)"$/ do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
