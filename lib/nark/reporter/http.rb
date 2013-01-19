d926d4e4d47bdf0c9180d84a3c702085006cbff8
Yomi Colledge <baphled@boodah.net>
2013-01-19 11:47:08 +0000

    Add documentation

--- lib/nark/reporter/http.rb
+++ lib/nark/reporter/http.rb
@@ -2,12 +2,30 @@ require 'sinatra'
 
 module Nark
   module Reporter
+    #
+    # HTTP Report layer
+    #
+    # Used to provide a basic HTTP layer.
+    #
+    # This gives users and developers the opportunity to extract information
+    # from their plugin and use it in other places.
+    #
     class HTTP < Sinatra::Base
 
+      #
+      # GET /nark/available_plugins
+      #
+      # Returns an array of plugin's that are currently being used
+      #
       get "/nark/available_plugins" do
         JSON.pretty_generate :plugins => Nark.available_plugins
       end
 
+      #
+      # GET /nark/:plugin_method
+      #
+      # Exposes the plugins class method and returns the data.
+      #
       get "/nark/:plugin_method" do
         if Nark::Plugin.defined_methods.include? params[:plugin_method].to_sym
           JSON.pretty_generate Nark.public_send params[:plugin_method]
