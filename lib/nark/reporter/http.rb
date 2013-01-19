0592642edfb058ddd72bf7637f0f35c3b6217671
Yomi Colledge <baphled@boodah.net>
2013-01-19 11:43:34 +0000

    Remove the port, it's not being used

--- lib/nark/reporter/http.rb
+++ lib/nark/reporter/http.rb
@@ -3,7 +3,6 @@ require 'sinatra'
 module Nark
   module Reporter
     class HTTP < Sinatra::Base
-      set :port, 9494
 
       get "/nark/available_plugins" do
         JSON.pretty_generate :plugins => Nark.available_plugins
