661dfe5c7bd063b09fafc92227ece079b24d5a0d
Yomi Colledge <baphled@boodah.net>
2013-01-20 01:09:54 +0000

    Add pending spec to cover the future configuration functionality

--- spec/nark_spec.rb
+++ spec/nark_spec.rb
@@ -1,6 +1,15 @@
 require "spec_helper"
 
 describe Nark do
+  it "sets event handler for the middleware"
+
   describe "#configure" do
+    it "allows to to add a reporter"
+    it "define the plugin destination path"
+    it "overrides the event handler for the middleware"
+
+    context "HTTP reporter" do
+      it "define root path"
+    end
   end
 end
