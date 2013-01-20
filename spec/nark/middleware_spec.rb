fdb94e19b5fafe9fb1cf566c24a5fab1796c3dcc
Yomi Colledge <baphled@boodah.net>
2013-01-20 00:49:41 +0000

    Cover initialising the middleware

--- spec/nark/middleware_spec.rb
+++ spec/nark/middleware_spec.rb
@@ -2,11 +2,24 @@ require "spec_helper"
 
 describe Nark::Middleware do
   include Rack::Test::Methods
+  let(:target_app) { mock('The target application') }
 
   def app
-    @target_app = mock('The target application')
-    @target_app.stub(:call).and_return([200, {}, "Target application"])
-    Nark::Middleware.new(@target_app)
+    target_app.stub(:call).and_return([200, {}, "Target application"])
+    Nark::Middleware.new(target_app)
+  end
+
+  describe "#new" do
+    it "stores the application" do
+      middleware = Nark::Middleware.new target_app
+      middleware.app.should eql target_app
+    end
+
+    it "stores the event handler" do
+      event_handler = stub(:CustomEventHandler, trigger: stub)
+      middleware = Nark::Middleware.new target_app, event_handler
+      middleware.event_handler.should eql event_handler
+    end
   end
 
   describe "#call" do
