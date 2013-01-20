6ce03d511ebdb451a5525f401e76aad67e7b91a7
Yomi Colledge <baphled@boodah.net>
2013-01-20 02:15:44 +0000

    Update documentation for the new functionality

--- lib/nark/plugin.rb
+++ lib/nark/plugin.rb
@@ -28,10 +28,20 @@ module Nark
         modules.collect { |plugin| plugin.to_s.underscore }.sort
       end
 
+      #
+      # Loads all the plugins from the define plugin path
+      #
+      # This is used to resolve the issue of having to rely on the user to require the plugin manually.
+      #
       def load_plugins
         Dir["#{defined_plugin_path}/*.rb"].each { |plugin| eval File.read(plugin) }
       end
 
+      #
+      # Stores the path of the plugins
+      #
+      # TODO: Allow the user to set this path thier selves
+      #
       def defined_plugin_path
         File.absolute_path File.join File.dirname(__FILE__), "..", "..", 'plugins'
       end
