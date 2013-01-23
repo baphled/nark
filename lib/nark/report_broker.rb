a04a07b1251bcdb7849e257cd21ea192db264382
Yomi Colledge <baphled@boodah.net>
2013-01-23 16:07:48 +0000

    Add documentation

--- lib/nark/report_broker.rb
+++ lib/nark/report_broker.rb
@@ -1,6 +1,18 @@
 require_relative 'reporter/http'
 
 module Nark
+  #
+  # Handles which reporters should be included in the middle ware.
+  #
+  # This is used by Nark to determine whether any reporters are needed before
+  # setting up the application.
+  #
+  # This functionality relies on the fact that reporters are setup via Nark's
+  # configuration block.
+  #
+  # If no reporters are defined within the configuration block then no
+  # reporters will be added to the middleware.
+  #
   module ReportBroker
     module ClassMethods
       @@reporters = []
