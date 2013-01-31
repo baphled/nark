9b000cce8fc9c4df76fa8f8440a73030cb21a6c5
Yomi Colledge <baphled@boodah.net>
2013-01-31 23:21:27 +0000

    Move the configuration specs to the right place

--- spec/nark/configuration_spec.rb
+++ spec/nark/configuration_spec.rb
@@ -9,6 +9,22 @@ describe Nark::Configuration do
     Nark::Configuration.settings_path.should eql 'config/nark.yml'
   end
 
+  describe "#configure" do
+    it "allows for setting the config path" do
+      Nark.settings_path = 'nark.yml'
+      Nark.settings_path.should eql 'nark.yml'
+    end
+
+    context "adding a reporter" do
+      it "has access to the reporter object" do
+        Nark.configure do |c|
+          c.reporters = [:HTTP]
+        end
+        Nark.reporters.should include Nark::Reporter::HTTP
+      end
+    end
+  end
+
   describe "#settings" do
     it "returns an empty hash if no config file was found" do
       Nark::Configuration.settings_path = ''
--- spec/nark_spec.rb
+++ spec/nark_spec.rb
@@ -16,21 +16,4 @@ describe Nark do
       Nark.should respond_to :plugins_path
     end
   end
-
-  describe "#configure" do
-    it "allows for setting the config path" do
-      Nark.settings_path = 'nark.yml'
-      Nark.settings_path.should eql 'nark.yml'
-    end
-
-    context "adding a reporter" do
-      it "has access to the reporter object" do
-        Nark.configure do |c|
-          c.reporters = [:HTTP]
-        end
-        Nark.reporters.should include Nark::Reporter::HTTP
-      end
-    end
-
-  end
 end
