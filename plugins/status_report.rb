a448b83900575cc11e610166e23bdbaf1a3b45e7
Yomi Colledge <baphled@boodah.net>
2014-09-07 21:08:51 +0100

    Rectify typo in plugin variable name

--- plugins/status_report.rb
+++ plugins/status_report.rb
@@ -1,7 +1,7 @@
 Nark::Plugin.define :status_report do |plugin|
-  plugin.variables :statues => []
+  plugin.variables :statuses => []
 
   plugin.add_hook :after_call do |status_code, header, body, env|
-    plugin.statues << {:status => status_code, :path => env['PATH_INFO']}
+    plugin.statuses << {:status => status_code, :path => env['PATH_INFO']}
   end
 end
